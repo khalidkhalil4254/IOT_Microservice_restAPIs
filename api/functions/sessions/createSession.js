17b9586f76bd49c5ac8f5723c8422b0fa3d7a814
khalid khalil <kalidkalil4254@gmail.com>
2023-06-29 17:01:55 +0300

    adding some changes

--- api/functions/sessions/createSession.js
+++ api/functions/sessions/createSession.js
@@ -1,7 +1,65 @@
 const res = require('../../common/responses');
-const conf = require('../../common/config');
-const mysql=require('mysql');
+const config = require('../../common/config');
+const mysql = require('mysql');
 
 module.exports.handler=async(event, context, callback)=>{
+
+    const data = JSON.parse(event.body);
+    const speaker = data.speaker;
+    const title = data.title;
+    const company = data.company;
+    const startTime = data.startTime;
+    const endTime = data.endTime;
+
+
+    console.log(`Data: ${data}`);
+
+    //creating database connections:
+    const connectionPool = mysql.createPool({
+        host: config.conf.db_host,
+        user: config.conf.db_username,
+        password: config.conf.db_password,
+        database: config.conf.db_name,
+    });
+
+
+    const newSessionDetails = {
+        speaker,
+        title,
+        company,
+        startTime,
+        endTime
+    }
+
+    const query = `select * from sessions;`;
+
+
+    const results = await new Promise((resolve, reject) => {
+        pool.query(query, newSessionDetails, (error, results, fields) => {
+            if (error) {
+                console.error(error);
+                reject(results);
+            }else{
+                console.log('Reading the found items from the table...');
+                resolve(results);
+            }
     
+        });
+      });
+
+      // Close the connection pool
+      pool.end();
+
+
+      if(results.length>0){
+        console.log(`Result: ${results}`);
+        return res._200;
+      }
+
+      else{
+        console.log(`Not found any resources!`);
+        return res._404;
+      }
+
+
 }
--- api/functions/sessions/readSession.js
+++ api/functions/sessions/readSession.js
@@ -1,6 +1,48 @@
 const res = require('../../common/responses');
-const conf = require('../../common/config');
+const config = require('../../common/config');
+const mysql = require('mysql');
 
 module.exports.handler=async(event, context, callback)=>{
+
+    //creating database connections:
+    const connectionPool = mysql.createPool({
+        host: config.conf.db_host,
+        user: config.conf.db_username,
+        password: config.conf.db_password,
+        database: config.conf.db_name,
+    });
+
+
+
+    const query = `select * from sessions;`;
+
+
+    const results = await new Promise((resolve, reject) => {
+        pool.query(query, (error, results, fields) => {
+            if (error) {
+                console.error(error);
+                reject(results);
+            }else{
+                console.log('Reading the found items from the table...');
+                resolve(results);
+            }
     
+        });
+      });
+
+      // Close the connection pool
+      pool.end();
+
+
+      if(results.length>0){
+        console.log(`Result: ${results}`);
+        return res._200;
+      }
+
+      else{
+        console.log(`Not found any resources!`);
+        return res._404;
+      }
+
+
 }
