d4143f4231e3d2f81d34a6cb2367a20c89bfb644
Khalid Khalil <kalidkalil4254@gmail.com>
2023-06-29 16:12:53 +0300

    Dev (#3)
    * adding readme & some changes (#2)
    
    * Dev (#1)
    
    * refactore-to-microservice
    
    * Update README.md
    
    Small initial description about the project
    
    * Update README.md
    
    * Delete postman(screens) directory
    
    * Delete serverless_iot_rfid.sql
    
    * Update README.md
    
    * add-sessions-svc
    
    * add sessions-svc
    
    * Update README.md

---
+++ api/functions/sessions/createSession.js
@@ -0,0 +1,7 @@
+const res = require('../../common/responses');
+const conf = require('../../common/config');
+const mysql=require('mysql');
+
+module.exports.handler=async(event, context, callback)=>{
+    
+}
---
+++ api/functions/sessions/readSession.js
@@ -0,0 +1,6 @@
+const res = require('../../common/responses');
+const conf = require('../../common/config');
+
+module.exports.handler=async(event, context, callback)=>{
+    
+}
